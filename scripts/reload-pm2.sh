2c8e0103f9c89348fae6776a05db0de3d62381db
Juanfcolopez <jflopez2@uc.cl>
2020-11-08 03:45:09 -0300

    Add changes in reaload

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -2,5 +2,5 @@
 cd ~/aws-codedeploy
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml down
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml build
-sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml up
+sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml up -d
 pm2 startOrReload ecosystem.config.js
