0a25ee48333e421639760e9517b2c997f70109d5
Juanfcolopez <jflopez2@uc.cl>
2020-11-08 03:30:21 -0300

    Add startOrReload command

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -3,3 +3,4 @@ cd ~/aws-codedeploy
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml down
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml build
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml up
+pm2 startOrReload ecosystem.config.js
