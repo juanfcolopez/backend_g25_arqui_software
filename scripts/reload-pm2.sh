cba8f293dc6f40474f0c2f19f74adcad6b5927f7
Juanfcolopez <jflopez2@uc.cl>
2020-11-09 02:45:43 -0300

    Add migrate command in reload-pm2.sh

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -3,5 +3,6 @@ cp ~/local_env.yml ~/aws-codedeploy/config
 cd ~/aws-codedeploy
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml down
 sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml build
-sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml up -d
+sudo docker-compose -f /home/ubuntu/aws-codedeploy/ run web rails db:migrate
+sudo docker-compose -f /home/ubuntu/aws-codedeploy/docker-compose.yml up -d  
 pm2 startOrReload ecosystem.config.js
